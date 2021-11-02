d17056db97141e311ed6c457f7902e446bcf1bf9
yerin <uyttyu7532@naver.com>
2021-11-02 16:53:13 +0900

    버튼 누르면 확장되는 아이템 생성

--- week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/com/codelab/basicscodelab2/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/com/codelab/basicscodelab2/MainActivity.kt
@@ -3,11 +3,12 @@ package com.codelab.basicscodelab2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.codelab.basicscodelab2.ui.theme.BasicsCodelab2Theme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +16,56 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BasicsCodelab2Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
+fun MyApp() {
+    Greetings()
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    val expanded = remember { mutableStateOf(false) }
+
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+    Surface(color = MaterialTheme.colors.primary, modifier = Modifier.padding(5.dp)) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1.0f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello!")
+                Text(text = name)
+            }
+            OutlinedButton(
+                onClick = { expanded.value = !expanded.value },
+                modifier = Modifier.padding(2.dp)
+            ) {
+                Text(if (expanded.value) "Show Less" else "Show More")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greetings(names: List<String> = listOf("Android", "Google", "JetBrain")) {
+    Column {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BasicsCodelab2Theme {
-        Greeting("Android")
+        MyApp()
     }
 }
